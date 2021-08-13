c02b57fab75105bbeb4e56d8b24d87dbf0f7e5d9
Cody Lacey <cody605@gmail.com>
2021-08-13 12:38:20 -0500

    mail

--- mail.php
+++ mail.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD
-	$emailTo = "cody@codylacey.com"; // Enter your email for feedbacks here
-=======
-	$emailTo = "cody605@gmail.com"; // Enter your email for feedbacks here.
->>>>>>> b2ed29c143528c6cc6dc19baa23e59cb34c73cd1
+	$emailTo = "cody@codylacey.com"; // Enter your email for feedbacks here.
 
 	$headers = "MIME-Version: 1.0\r\n";
 	$headers .= "Content-type: text/html; charset=utf-8\r\n";
